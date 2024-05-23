dc0b919fa86fec412648a7fe861c0e87a2e35c0b
Niket mishra <mishraniket452@gmail.com>
2024-05-24 02:59:42 +0530

    Login

--- src/travel/management/system/Splash.java
+++ src/travel/management/system/Splash.java
@@ -22,7 +22,7 @@ import java.awt.*;
     public void run (){
         try{
             Thread.sleep(7000);
-            //new Login();
+            new Login();
             setVisible(false);
         } catch (Exception e) {
 
